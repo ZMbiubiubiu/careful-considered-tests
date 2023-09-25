3b9a1cb03217056f4ac0b238d78fdf932a969c79
bingozhangmeng <bingozhangmeng@didiglobal.com>
2023-09-25 16:14:59 +0800

    nil slice vs empty slice

--- slice_test.go
+++ slice_test.go
@@ -16,6 +16,16 @@ func TestReuseSlice(t *testing.T) {
 
 func TestSliceArrayAddress(t *testing.T) {
 	var nums = []int{1, 2, 3}
-	fmt.Printf("%T slice address: %p\t arrary address: %p\n", nums, &nums, nums)
+	t.Logf("%T slice address: %p\t arrary address: %p\n", nums, &nums, nums)
 	assert.Equal(t, fmt.Sprintf("%p", nums), fmt.Sprintf("%p", &nums[0]))
+}
+
+func TestNilAndEmptySlice(t *testing.T) {
+	var s1 []string         // nil
+	s2 := []string{}        // empty
+	s3 := make([]string, 0) // empty, equivalent to s2 := []string{}
+
+	t.Logf("s1 is nil: %t, len: %d, cap: %d, backend array addr: %p\n", s1 == nil, len(s1), cap(s1), s1)
+	t.Logf("s2 is nil: %t, len: %d, cap: %d, backend array addr: %p\n", s2 == nil, len(s2), cap(s2), s2)
+	t.Logf("s3 is nil: %t, len: %d, cap: %d, backend array addr: %p\n", s3 == nil, len(s3), cap(s3), s3)
 }
