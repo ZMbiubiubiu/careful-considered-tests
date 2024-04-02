c5b633278e0c5e023717ddc43c820e07d3f9b6d6
bingozhangmeng <bingozhangmeng@didiglobal.com>
2024-04-02 14:57:06 +0800

    dynamic goroutines for deal data

--- code-questions/meanwhile_fixed_goroutine.go
+++ code-questions/meanwhile_fixed_goroutine.go
@@ -7,8 +7,8 @@ import (
 )
 
 var (
-	ch = make(chan struct{}, 2)
-	wg sync.WaitGroup
+	ch  = make(chan struct{}, 2)
+	wg1 sync.WaitGroup
 )
 
 func doSomeWork(i int) {
@@ -16,16 +16,39 @@ func doSomeWork(i int) {
 	time.Sleep(time.Second)
 
 	<-ch
-	wg.Done()
+	wg1.Done()
+}
+
+// runBoundedTask 起maxTaskNum个协程共同处理任务
+// 实现简单，但是有可能数据太多，导致处理受到阻塞；数据太小，协程空闲
+func runBoundedTask(dataChan <-chan int, maxTaskNum int) {
+	var wg sync.WaitGroup
+	wg.Add(maxTaskNum)
+
+	for i := 0; i < maxTaskNum; i++ {
+		go func() {
+			defer wg.Done()
+
+			for data := range dataChan {
+				func(data int) {
+
+					// do something
+					time.Sleep(3 * time.Second)
+				}(data)
+			}
+		}()
+	}
+
+	wg.Wait()
 }
 
 func main() {
 	total := 20
-	wg.Add(total)
+	wg1.Add(total)
 	for i := 0; i < total; i++ {
 		ch <- struct{}{}
 		go doSomeWork(i)
 	}
 	close(ch)
-	wg.Wait()
+	wg1.Wait()
 }
