3d8f65d04abbe5d0ba0958fb350ed44835fd540f
上杉夏香 <390047718@qq.com>
2024-04-20 08:48:11 +0800

    add geeorm raw

---
+++ GeeORM/geeorm.go
@@ -0,0 +1,39 @@
+package geeorm
+
+import (
+	"database/sql"
+
+	"geeorm/log"
+	"geeorm/session"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	// Send a ping to make sure the database connection is alive.
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+func (e *Engine) Close() {
+	if err := e.db.Close(); err != nil {
+		log.Error("Engine Close failed")
+	}
+}
+
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db)
+
+}
---
+++ GeeORM/session/raw.go
@@ -0,0 +1,63 @@
+package session
+
+import (
+	"database/sql"
+	"strings"
+
+	"geeorm/log"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+// Clear 如此可重复利用session
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ") // why
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
+
+// 下面是对Exec、Query、QueryRow的封装
+// 统一打日志
+// 执行完成后，调用Clear方法，清空Session.sql和Session.sqlVars两个变量
+
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err.Error())
+	}
+	return
+}
+
+func (s *Session) Query() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err.Error())
+	}
+	return
+}
+
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
