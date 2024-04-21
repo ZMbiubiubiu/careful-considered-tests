8a2e6ed5e7f251092861022a0842381901467370
上杉夏香 <390047718@qq.com>
2024-04-21 15:34:09 +0800

    add geeorm chain

--- GeeORM/session/record.go
+++ GeeORM/session/record.go
@@ -1,6 +1,10 @@
+// tips:下面的方法对Model的解析有些边界不清晰
+// 有的需要在方法内部调用Model方法，有的不需要
+
 package session
 
 import (
+	"errors"
 	"reflect"
 
 	"geeorm/clause"
@@ -9,8 +13,11 @@ import (
 func (s *Session) Insert(values ...interface{}) (int64, error) {
 	recordValues := make([]interface{}, 0)
 	for _, value := range values {
+		// 最多只会解析一次model，go struct -> db schema
 		table := s.Model(value).RefTable()
+		// 幂等set
 		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
+		// insert into (xxx) values() 中values后面的参数
 		recordValues = append(recordValues, table.RecordValues(value))
 	}
 
@@ -24,12 +31,14 @@ func (s *Session) Insert(values ...interface{}) (int64, error) {
 	return result.RowsAffected()
 }
 
+// Find 通用的select实现
 func (s *Session) Find(values interface{}) error {
 	destSlice := reflect.Indirect(reflect.ValueOf(values))
 	destType := destSlice.Type().Elem()
 	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable()
 
 	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
+	// 调用方自己去设置WHERE、LIMIT、ORDER BY子句，Find只负责组装这些子句
 	sql, vars := s.clause.Build(clause.SELECT, clause.WHERE, clause.ORDERBY, clause.LIMIT)
 	rows, err := s.Raw(sql, vars...).QueryRows()
 	if err != nil {
@@ -48,4 +57,80 @@ func (s *Session) Find(values interface{}) error {
 		destSlice.Set(reflect.Append(destSlice, dest))
 	}
 	return rows.Close()
+}
+
+// Update 接受 2 种入参，平铺开来的键值对和 map 类型的键值对
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	m, ok := kv[0].(map[string]interface{})
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
+	}
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+// Delete records with where clause
+func (s *Session) Delete() (int64, error) {
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	return result.RowsAffected()
+}
+
+// Count records with where clause
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
+}
+
+// Limit adds limit condition to clause
+func (s *Session) Limit(num int) *Session {
+	s.clause.Set(clause.LIMIT, num)
+	return s
+}
+
+// Where adds limit condition to clause
+// desc：包含?占位符的语句，args：指定?占位符对应的变量值
+func (s *Session) Where(desc string, args ...interface{}) *Session {
+	var vars = append([]interface{}(nil), desc)
+	vars = append(vars, args...)
+	s.clause.Set(clause.WHERE, vars...)
+	return s
+}
+
+// OrderBy adds order by condition to clause
+func (s *Session) OrderBy(desc string) *Session {
+	s.clause.Set(clause.ORDERBY, desc)
+	return s
+}
+
+func (s *Session) First(value interface{}) error {
+	dest := reflect.Indirect(reflect.ValueOf(value))
+	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
+	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
+		return err
+	}
+	if destSlice.Len() == 0 {
+		return errors.New("NOT FOUND")
+	}
+
+	dest.Set(destSlice.Index(0))
+	return nil
 }
