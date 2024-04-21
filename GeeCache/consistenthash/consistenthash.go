15b2849ba85144d60a3bf80f22998225133df29d
上杉夏香 <390047718@qq.com>
2024-04-21 19:50:25 +0800

    modify geecache consistent hash

--- GeeCache/consistenthash/consistenthash.go
+++ GeeCache/consistenthash/consistenthash.go
@@ -6,23 +6,24 @@ import (
 	"strconv"
 )
 
-// Hash 采取依赖注入的方式，允许用于替换成自定义的 Hash 函数，也方便测试时替换，默认为 crc32.ChecksumIEEE 算法。
+// Hash 采取依赖注入的方式，默认为 crc32.ChecksumIEEE 算法
+// 允许用于替换成自定义的 Hash 函数，方便测试时替换
 type Hash func(data []byte) uint32
 
 // Map 一致性hash函数的核心结构
 type Map struct {
-	hash     Hash
-	replicas int            // 虚拟节点的倍数
-	keys     []int          // 虚拟节点hash值组成的hash环
-	hashMap  map[int]string // 虚拟节点hash值与节点的映射关系
+	hash      Hash
+	replicas  int            // 节点的倍数，一个节点可产生replicas个虚拟节点，解决数据倾斜问题
+	vnodes    []int          // 虚拟节点hash值组成的hash环
+	hash2node map[int]string // 虚拟节点hash值与节点的映射关系
 }
 
 func New(replicas int, fn Hash) *Map {
 	m := &Map{
-		hash:     fn,
-		replicas: replicas,
-		keys:     nil,
-		hashMap:  make(map[int]string),
+		hash:      fn,
+		replicas:  replicas,
+		vnodes:    nil,
+		hash2node: make(map[int]string),
 	}
 
 	if m.hash == nil {
@@ -37,27 +38,30 @@ func (m *Map) Add(nodes ...string) {
 	for _, node := range nodes {
 		// 每个真实节点对应replicas个虚拟节点
 		for i := 0; i < m.replicas; i++ {
-			h := int(m.hash([]byte(strconv.Itoa(i) + node)))
-			m.keys = append(m.keys, h)
-			m.hashMap[h] = node
+			vnode := strconv.Itoa(i) + node
+			h := int(m.hash([]byte(vnode)))
+			m.vnodes = append(m.vnodes, h)
+			m.hash2node[h] = node
 		}
 	}
-	sort.Ints(m.keys)
+
+	// 排序很重要
+	sort.Ints(m.vnodes)
 }
 
 // Get 给定key返回需要的节点
 func (m *Map) Get(key string) string {
-	if len(m.keys) == 0 {
+	if len(m.vnodes) == 0 {
 		return ""
 	}
 
 	hash := int(m.hash([]byte(key)))
 
-	idx := sort.Search(len(m.keys), func(i int) bool {
-		return m.keys[i] >= hash
+	idx := sort.Search(len(m.vnodes), func(i int) bool {
+		return m.vnodes[i] >= hash
 	})
 
-	idx = idx % len(m.keys)
+	idx = idx % len(m.vnodes)
 
-	return m.hashMap[m.keys[idx]]
+	return m.hash2node[m.vnodes[idx]]
 }
