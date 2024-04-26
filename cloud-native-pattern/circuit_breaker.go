49f1f78afe120caed84888549d962ccf2888acfd
上杉夏香 <390047718@qq.com>
2024-04-26 21:56:45 +0800

    add circuit-breaker

---
+++ cloud-native-pattern/circuit_breaker.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// what is a closure
+// A closure is a nested function that has access to the variables
+// of its parent function, even after the parent has executed
+
+type Circuit func(ctx context.Context) (string, error)
+
+func Breaker(circuit Circuit, failureThreshold uint) Circuit {
+	var consecutiveFailures int = 0 // 连续失败的次数
+	var lastAttempt = time.Now()
+	var mu sync.RWMutex // 保护consecutiveFailures
+
+	return func(ctx context.Context) (string, error) {
+		mu.RLock()
+
+		d := consecutiveFailures - int(failureThreshold)
+		if d >= 0 {
+			shouldRetryAt := lastAttempt.Add(time.Second * 2 << d) // 指数回退的重试策略
+			if time.Now().Before(shouldRetryAt) {
+				mu.RUnlock()
+				return "", errors.New("service unreachable")
+			}
+		}
+		mu.RUnlock()
+
+		response, err := circuit(ctx)
+		mu.Lock()
+		defer mu.Unlock()
+
+		lastAttempt = time.Now()
+
+		if err != nil {
+			consecutiveFailures++
+			return response, err
+		}
+
+		consecutiveFailures = 0
+		return response, nil
+	}
+}
+
+func circuitFunc(ctx context.Context) (string, error) {
+	return "", errors.New("oh no")
+}
+
+func main() {
+	threshold := 5
+	breaker := Breaker(circuitFunc, uint(threshold))
+	for i := 0; i < 7; i++ {
+		_, err := breaker(context.Background())
+		if i < threshold {
+			fmt.Println(err.Error() == "oh no")
+		} else {
+			fmt.Println(err.Error() == "service unreachable")
+		}
+	}
+
+}
