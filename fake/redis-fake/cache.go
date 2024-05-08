70aeff4a77cf00b470d8e45a72f3f89be83a6b92
bingozhangmeng <bingozhangmeng@didiglobal.com>
2024-05-08 14:05:55 +0800

    redis fake case

--- fake/redis-fake/cache.go
+++ fake/redis-fake/cache.go
@@ -1,1 +1,37 @@
-package redis_fake
+package main
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/astaxie/beego"
+	_ "github.com/astaxie/beego/cache/redis"
+	"github.com/go-redis/redis/v8"
+)
+
+var (
+	rdb  *redis.Client
+	once sync.Once
+	ctx  = context.Background()
+)
+
+func InitRedisClient() {
+	once.Do(func() {
+		rdb = redis.NewClient(&redis.Options{
+			Addr: beego.AppConfig.String("redis_addr"),
+		})
+	})
+}
+
+func GetString(key string) (value string, err error) {
+	v, err := rdb.Get(ctx, key).Result()
+	if err != nil && err != redis.Nil {
+		return "", err
+	}
+	return v, nil
+}
+
+func SetString(key, value string, time time.Duration) (err error) {
+	return rdb.Set(ctx, key, value, time).Err()
+}
--- fake/redis-fake/cache_test.go
+++ fake/redis-fake/cache_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alicebob/miniredis/v2"
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	// redis-fake server
+	miniRedis *miniredis.Miniredis
+)
+
+func TestMain(m *testing.M) {
+	// 启动redis-fake服务
+	miniRedis, _ = miniredis.Run()
+
+	// 连接redis-fake服务
+	rdb = redis.NewClient(&redis.Options{
+		Addr: miniRedis.Addr(),
+	})
+	os.Exit(m.Run())
+}
+
+func TestCache(t *testing.T) {
+	// 测试未过期
+	err := SetString("foo", "bar", 10*time.Second)
+	assert.Nil(t, err)
+	got, err := GetString("foo")
+	assert.Nil(t, err)
+	assert.Equal(t, "bar", got)
+
+	// 测试过期
+	err = SetString("foo", "bar", 10*time.Second)
+	assert.Nil(t, err)
+	// 快进11s
+	miniRedis.FastForward(11 * time.Second)
+	got, err = GetString("foo")
+	assert.Nil(t, err)
+	assert.Equal(t, "", got)
+}
